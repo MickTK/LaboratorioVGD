8f201810851865fa8b7352dbf82960bf4207b858
GabryFourth <gabriel140300@gmail.com>
2022-05-27 16:09:49 +0200

    Scene Main

---
+++ Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OpenGame(){
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OpenOptions(){
+
+    }
+
+    public void QuitGame(){
+        // save any game data here
+        #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
